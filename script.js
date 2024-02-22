03d8291361b2f691b1d60461510bd26637737626
m0x0m0x <mx0mx@tuta.io>
2024-02-22 22:33:45 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+"use strict";
+
+// // This targets an element
+// console.log(document.querySelector(".message").textContent);
+
+// // Changing the text content after targeting with the command below
+// document.querySelector(".message").textContent = "🍑 Correct Number";
+// console.log(document.querySelector(".message").textContent);
+
+// // Change Secret number
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// // Change the input field
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
